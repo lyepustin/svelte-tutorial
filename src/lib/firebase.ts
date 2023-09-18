3fe8243c72d505fe17dba19f4965a1abae98c0c6
Torres1080 <torres10801211@gmail.com>
2023-09-18 22:08:01 +0200

    Damn bro, tailwind

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,33 +1,33 @@
-import {
-  PUBLIC_FIREBASE_API_KEY,
-  PUBLIC_FIREBASE_AUTH_DOMAIN,
-  PUBLIC_FIREBASE_PROJECT_ID,
-  PUBLIC_FIREBASE_STORAGE_BUCKET,
-  PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  PUBLIC_FIREBASE_APP_ID
-} from '$env/static/public';
-import { initializeApp, getApps } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// import {
+//   PUBLIC_FIREBASE_API_KEY,
+//   PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   PUBLIC_FIREBASE_PROJECT_ID,
+//   PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   PUBLIC_FIREBASE_APP_ID
+// } from '$env/static/public';
+// import { initializeApp, getApps } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
  
-import { GoogleAuthProvider } from "firebase/auth";
+// import { GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: PUBLIC_FIREBASE_API_KEY,
-  authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: PUBLIC_FIREBASE_APP_ID
-};
+// const firebaseConfig = {
+//   apiKey: PUBLIC_FIREBASE_API_KEY,
+//   authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: PUBLIC_FIREBASE_APP_ID
+// };
  
-let firebaseApp;
-const GoogleProvider = new GoogleAuthProvider();
-GoogleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+// let firebaseApp;
+// const GoogleProvider = new GoogleAuthProvider();
+// GoogleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
-if (!getApps().length) {
-  firebaseApp = initializeApp(firebaseConfig);
-}
+// if (!getApps().length) {
+//   firebaseApp = initializeApp(firebaseConfig);
+// }
 
-const firebaseAuth = getAuth(firebaseApp);
+// const firebaseAuth = getAuth(firebaseApp);
  
-export { firebaseApp, firebaseAuth, GoogleProvider}
+// export { firebaseApp, firebaseAuth, GoogleProvider}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,31 @@
 <script lang="ts">
-  import session from '$lib/session';
+  // import session from '$lib/session';
 	import { goto } from '$app/navigation';
-	import { signInWithPopup } from 'firebase/auth';
-  import { firebaseAuth, GoogleProvider } from '$lib/firebase';
+	// import { signInWithPopup } from 'firebase/auth';
+  // import { firebaseAuth, GoogleProvider } from '$lib/firebase';
 
   let email: string;
   let password: string;
 
   async function signIn() {
-    const result = await signInWithPopup(firebaseAuth, GoogleProvider);
-    console.log(result);
+    // const result = await signInWithPopup(firebaseAuth, GoogleProvider);
+    // console.log(result);
     goto('/dashboard');
   }
 </script>
 
-<div class="wrapper">
-  <div class="w-full max-w-xs">
-    <form class="px-8 pt-6 pb-8 bg-white shadow-md" on:submit={signIn}>
-      <div class="mb-4">
-        <label for="email">Email</label>
-        <input bind:value={email} class="input-field" id="email" type="text" placeholder="name@acme.com"/>
-      </div>
-      <div class="mb-6">
-        <label for="password">Password</label>
-        <input bind:value={password} class="input-field" id="password" type="password" placeholder="******************"/>
-      </div>
-      <div class="">
-        <button type="submit">Sign In</button>
-      </div>
-    </form>
-  </div>
+<div class="w-screen h-screen justify-center flex items-center bg-gray-100">
+  <form class="p-8 bg-white shadow-md rounded-lg" on:submit={signIn}>
+    <div class="mb-4">
+      <label for="email">Email</label>
+      <input bind:value={email} id="email" type="text" placeholder="name@acme.com"/>
+    </div>
+    <div class="mb-6">
+      <label for="password">Password</label>
+      <input bind:value={password} id="password" type="password" placeholder="******************"/>
+    </div>
+    <div class="">
+      <button type="submit">Sign In</button>
+    </div>
+  </form>
 </div>
